bac7076b817cfe7be1a75286f6febec5f8ff5b8d
Denis Bondarchuk <denis@kaizen-design.ru>
2024-01-19 00:54:36 +0300

    Delete dist directory

--- dist/js/scripts.js
+++
@@ -1,7 +0,0 @@
-//libraries like jquery etc
-window.addEventListener("DOMContentLoaded", () => {
-  //  Prevent jump to top on "#" links
-  document.querySelectorAll("a[href='#']").forEach(link => {
-    link.addEventListener("click", e => e.preventDefault());
-  });
-});
