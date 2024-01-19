df4a56691ac7d66137700eae0479b085dddd24fb
kaizen-design <denis@kaizen-design.ru>
2024-01-19 19:11:53 +0300

    Mobile menu

--- dist/js/scripts.js
+++ dist/js/scripts.js
@@ -5,9 +5,35 @@ window.addEventListener("DOMContentLoaded", () => {
     link.addEventListener("click", e => e.preventDefault());
   });
 
+  //  Mobile Nav
+  initMobileMenu();
+
   //  Homepage
   initReviewsSlider();
 });
+
+function initMobileMenu() {
+  const $toggle = document.querySelector("#mobileMenuToggle");
+  
+  if (!$toggle) return;
+
+  const $icon = $toggle.getElementsByTagName("img");
+  const $menu = document.querySelector("#mobileNav");
+
+  const menuIcon = 'img/icons/menu.svg';
+  const closeIcon = 'img/icons/close.svg';
+
+  $toggle.addEventListener("click", e => {
+    const className = '-translate-y-full';
+    if ($menu.classList.contains(className)) {
+      $icon[0].setAttribute('src', closeIcon);
+      $menu.classList.remove('-translate-y-full')
+    } else {
+      $icon[0].setAttribute('src', menuIcon);
+      $menu.classList.add('-translate-y-full');
+    }
+  });
+}
 
 function initReviewsSlider() {  
   const $slider = document.querySelector(".reviewsSlider");
