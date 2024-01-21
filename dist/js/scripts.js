382d499bd4dc59010a9043e74c81a026ae18a3b3
kaizen-design <denis@kaizen-design.ru>
2024-01-21 17:27:49 +0300

    Popup modal added

--- dist/js/scripts.js
+++ dist/js/scripts.js
@@ -7,6 +7,8 @@ window.addEventListener("DOMContentLoaded", () => {
 
   //  Mobile Nav
   initMobileMenu();
+  initModals();
+  handleCallbackForm();
 
   //  Homepage
   initReviewsSlider();
@@ -140,4 +142,72 @@ function initContactPopover() {
     }
   });
   
+}
+
+function initModals() {
+  var openmodal = document.querySelectorAll('.modal-open')
+  for (var i = 0; i < openmodal.length; i++) {
+    openmodal[i].addEventListener('click', function(event){
+    event.preventDefault()
+    toggleModal()
+    })
+  }
+  
+  const overlay = document.querySelector('.modal-overlay')
+  overlay.addEventListener('click', toggleModal)
+  
+  var closemodal = document.querySelectorAll('.modal-close')
+  for (var i = 0; i < closemodal.length; i++) {
+    closemodal[i].addEventListener('click', toggleModal)
+  }
+  
+  document.onkeydown = function(evt) {
+    evt = evt || window.event
+    var isEscape = false
+    if ("key" in evt) {
+    isEscape = (evt.key === "Escape" || evt.key === "Esc")
+    } else {
+    isEscape = (evt.keyCode === 27)
+    }
+    if (isEscape && document.body.classList.contains('modal-active')) {
+    toggleModal()
+    }
+  };
+  
+  
+  function toggleModal () {
+    const body = document.querySelector('body')
+    const modal = document.querySelector('.modal')
+    modal.classList.toggle('opacity-0')
+    modal.classList.toggle('pointer-events-none')
+    body.classList.toggle('modal-active')
+  }
+}
+
+function handleCallbackForm() {
+  const $form = document.querySelector('.callback-form');
+
+  if (!$form) return;
+
+  $form.addEventListener('submit', e => {
+    e.preventDefault();
+
+    const $btn = $form.querySelector('button[type="submit"]');
+    const $formFieldset = $form.closest('.form-fieldset');
+    const $resultFieldset = $formFieldset.nextElementSibling;
+  
+    $btn.setAttribute('disabled', true);
+    $btn.textContent = 'Отправка...'
+
+    return new Promise(resolve => {
+      setTimeout(resolve, 3000);
+    })
+    .then(() => {
+      $formFieldset.classList.add('hidden');
+      $resultFieldset.classList.remove('hidden');
+    })
+    .finally(() => {
+          
+    });
+  })
 }
